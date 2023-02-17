d14f5a6bf38564396332bfa8e6bb29ad8fbcdf8e
Eric Mok <eric.mok@innopage.com>
2023-02-17 12:29:07 -0500

    Update Double extension with converting NumberString, with percentage

--- SwiftUICrypto/Extensions/Double.swift
+++ SwiftUICrypto/Extensions/Double.swift
@@ -8,6 +8,31 @@
 import Foundation
 
 extension Double {
+    
+    /// Convert Double to a Currency  with 2 decimal places
+    /// ```
+    /// Convert 1234.56 to $1,234.56
+    /// ```
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.locale = .current // default
+        formatter.currencyCode = "usd" // change currency
+        formatter.currencySymbol = "$" // change currency symbol
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    /// Convert Double to a Currency as String  with 2 decimal places
+    /// ```
+    /// Convert 1234.56 to "$1,234.56"
+    /// ```
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "$0.00"
+    }
    
     /// Convert Double to a Currency  with 2-6 decimal places
     /// ```
@@ -36,5 +61,21 @@ extension Double {
     func asCurrencyWith6Decimals() -> String {
         let number = NSNumber(value: self)
         return currencyFormatter6.string(from: number) ?? "$0.00"
+    }
+    
+    /// Convert Double to  String representation
+    /// ```
+    /// Convert 12.3456 to "12.34"
+    /// ```
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+    
+    /// Convert Double to  String representation with percentage
+    /// ```
+    /// Convert 12.3456 to "12.34%"
+    /// ```
+    func asPercentString() -> String {
+        return asNumberString() + "%"
     }
 }
