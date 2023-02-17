e8e64693747176b0deb9885527352352a8666624
Eric Mok <eric.mok@innopage.com>
2023-02-17 11:51:02 -0500

    Create PreviewProvider Extensions for testing data in preview

---
+++ SwiftUICrypto/Core/Home/View/CoinRowView.swift
@@ -0,0 +1,25 @@
+//
+//  CoinRowView.swift
+//  SwiftUICrypto
+//
+//  Created by MOK Lai Wo on 2023-02-17.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    let coin: CoinModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView(coin: dev.coin)
+    }
+}
+
+
