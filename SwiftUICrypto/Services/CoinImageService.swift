866bf05d61a57de5eaa5588c87a9765389b1926f
Eric Mok <eric.mok@innopage.com>
2023-02-20 12:37:39 -0500

    Create a LocalFileManager to save some coin images downloaded already, instead of downloading all again

--- SwiftUICrypto/Services/CoinImageService.swift
+++ SwiftUICrypto/Services/CoinImageService.swift
@@ -12,15 +12,27 @@ import Combine
 class CoinImageService {
     
     @Published var image: UIImage? = nil
-    var imageSubscription: AnyCancellable?
+    private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+        } else {
+            downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else { return }
         
         imageSubscription = NetworkingManager.download(url: url)
@@ -30,8 +42,10 @@ class CoinImageService {
             .sink { completion in
                 NetworkingManager.handleCompletion(completion: completion)
             } receiveValue: { [weak self] returnedImage in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = returnedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             }
     }
 }
---
+++ SwiftUICrypto/Utilities/LocalFileManager.swift
@@ -0,0 +1,72 @@
+//
+//  LocalFileManager.swift
+//  SwiftUICrypto
+//
+//  Created by MOK Lai Wo on 2023-02-20.
+//
+
+import Foundation
+import SwiftUI
+
+class LocalFileManager {
+    
+    static let instance = LocalFileManager()
+    
+    private init() { }
+    
+    func saveImage(image: UIImage, imageName: String, folderName: String) {
+        // Create Folder
+        createFolderIfNeeded(folderName: folderName)
+        
+        // Get Path for Image
+        guard
+            let data = image.pngData(),
+            let url = getURLForImage(imageName: imageName, folderName: folderName)
+        else { return }
+        
+        // Save image to path
+        do {
+            try data.write(to: url)
+        } catch let error {
+            print("Error saving image. Image Name: \(imageName). \(error)")
+        }
+    }
+    
+    func getImage(imageName: String, folderName: String) -> UIImage? {
+        guard
+            let url = getURLForImage(imageName: imageName, folderName: folderName),
+            FileManager.default.fileExists(atPath: url.path()) else {
+            return nil
+            
+        }
+        return UIImage(contentsOfFile: url.path())
+        
+    }
+    
+    private func createFolderIfNeeded(folderName: String) {
+        guard let url = getURLForFolder(folderName: folderName) else { return }
+        if !FileManager.default.fileExists(atPath: url.path()) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+            } catch let error {
+                print("Error creating Directory. Folder Name: \(folderName). \(error)")
+            }
+        }
+    }
+    
+    private func getURLForFolder(folderName: String) -> URL? {
+        guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        
+        return url.appending(path: folderName)
+    }
+    
+    private func getURLForImage(imageName: String, folderName: String) -> URL? {
+        guard let folderURL = getURLForFolder(folderName: folderName) else {
+            return nil
+        }
+        
+        return folderURL.appending(path: imageName + ".png")
+    }
+}
