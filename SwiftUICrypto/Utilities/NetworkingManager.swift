1cbf57deda950249387b8229c7bd700279b0ced4
Eric Mok <eric.mok@innopage.com>
2023-02-20 11:33:01 -0500

    Take out some reusable logic from DataService

---
+++ SwiftUICrypto/Utilities/NetworkingManager.swift
@@ -0,0 +1,51 @@
+//
+//  NetworkingManager.swift
+//  SwiftUICrypto
+//
+//  Created by MOK Lai Wo on 2023-02-17.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager {
+    
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url):
+                return "[] Bad response from URL: \(url)"
+            case .unknown:
+                return "[] Unknown Error."
+            }
+        }
+    }
+    
+    static func download(url: URL) -> AnyPublisher<Data,  Error> {
+       return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher() // Convert this Pulisher into anyPublisher, easier to return
+    }
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode <= 300 else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
+        return output.data
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
