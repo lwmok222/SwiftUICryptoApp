91b2971909f8f8f217bc0fd5bb99a896b38115be
Eric Mok <eric.mok@innopage.com>
2023-02-20 13:51:51 -0500

    Add SearchBarView into HomeView and add filter logic while searching text

--- SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -12,6 +12,8 @@ class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
+    @Published var searchText: String = ""
+    
     private let dataService = CoinDataService()
     private var cancellables = Set<AnyCancellable>()
     
@@ -20,10 +22,28 @@ class HomeViewModel: ObservableObject {
     }
     
    private func addSubscribers() {
-        dataService.$allCoins
-            .sink { [weak self] returnedCoins in
-                self?.allCoins = returnedCoins
-            }
-            .store(in: &cancellables)
+       // Update allCoins
+       $searchText
+           .combineLatest(dataService.$allCoins) // Will publish either searchText or allCoins changes value
+           .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+           .map(filterCoins)
+           .sink { [weak self] returnedCoins in
+               self?.allCoins = returnedCoins
+           }
+           .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { coin -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
