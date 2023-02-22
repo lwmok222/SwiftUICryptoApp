66a2d49c17da9b8a1443b8ff4f4acec4a56e4ff8
Eric Mok <eric.mok@innopage.com>
2023-02-22 12:19:25 -0500

    Update Save button logic in PortfolioView

--- SwiftUICrypto/Core/Home/View/PortfolioView.swift
+++ SwiftUICrypto/Core/Home/View/PortfolioView.swift
@@ -43,6 +43,11 @@ struct PortfolioView: View {
                    trailingNavButton
                 }
             }
+            .onChange(of: vm.searchText) { newValue in
+                if newValue == "" {
+                    removeSelectedCoin()
+                }
+            }
         }
     }
 }
@@ -58,13 +63,13 @@ extension PortfolioView {
     private var coinLogoList: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             LazyHStack(spacing: 10) {
-                ForEach(vm.allCoins) { coin in
+                ForEach(vm.searchText.isEmpty ?  vm.portfolioCoins : vm.allCoins) { coin in
                    CoinLogoView(coin: coin)
                         .frame(width: 75)
                         .padding(4)
                         .onTapGesture {
                             withAnimation(.easeIn) {
-                              selectedCoin = coin
+                                updateSelectedCoin(coin: coin)
                             }
                         }
                         .background {
@@ -76,6 +81,15 @@ extension PortfolioView {
             .frame(height: 120)
             .padding(.leading)
         }
+    }
+    
+    private func updateSelectedCoin(coin: CoinModel) {
+        selectedCoin = coin
+       if let portfolioCoin = vm.portfolioCoins.first(where: {$0.id == coin.id }), let amount = portfolioCoin.currentHoldings {
+          quantityText = "\(amount)"
+       } else {
+           quantityText = ""
+       }
     }
     
     private var portfolioInputSection: some View {
@@ -128,9 +142,10 @@ extension PortfolioView {
     }
     
     private func saveButtonPressed() {
-        guard let coin = selectedCoin else { return }
+        guard let coin = selectedCoin, let amount = Double(quantityText) else { return }
         
         // Save to Portfolio
+        vm.updatePortfolio(coin: coin, amount: amount)
         
         // show checkmark
         withAnimation(.easeIn) {
