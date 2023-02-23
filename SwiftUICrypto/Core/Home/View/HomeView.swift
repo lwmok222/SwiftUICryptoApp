c0035351f86826c5c3563fad1219edfcbf08713d
Eric Mok <eric.mok@innopage.com>
2023-02-23 11:10:17 -0500

    Create custom Navigation Link for lazy loading

--- SwiftUICrypto/Core/Home/View/HomeView.swift
+++ SwiftUICrypto/Core/Home/View/HomeView.swift
@@ -12,6 +12,8 @@ struct HomeView: View {
     @EnvironmentObject private var vm: HomeViewModel
     @State private var showPortfolio: Bool = false // animate right
     @State private var showPorfolioView: Bool = false // show new sheet
+    @State private var selectedCoin: CoinModel? = nil
+    @State private var showDetailView: Bool = false
     
     var body: some View {
         ZStack {
@@ -46,6 +48,13 @@ struct HomeView: View {
                 Spacer(minLength: 0)
                 
             }
+        }
+        .background {
+            NavigationLink(
+                destination: DetailLoadingView(coin: $selectedCoin),
+                isActive: $showDetailView) {
+                    EmptyView()
+                }
         }
     }
 }
@@ -98,6 +107,9 @@ extension HomeView {
             ForEach(vm.allCoins) { coin in
                 CoinRowView(coin: coin, showHoldingsColumn: false)
                     .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                    .onTapGesture {
+                        segue(coin: coin)
+                    }
             }
         }
         .listStyle(.plain)
@@ -112,9 +124,17 @@ extension HomeView {
             ForEach(vm.portfolioCoins) { coin in
                 CoinRowView(coin: coin, showHoldingsColumn: true)
                     .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                    .onTapGesture {
+                        segue(coin: coin)
+                    }
             }
         }
         .listStyle(.plain)
+    }
+    
+    private func segue(coin: CoinModel) {
+        selectedCoin = coin
+        showDetailView.toggle()
     }
     
     private var columnTitles: some View {
